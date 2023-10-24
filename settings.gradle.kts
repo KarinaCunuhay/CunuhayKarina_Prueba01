1e563db2a102593224dc0fb0718ec9125c8c9938
Karina Cunuhay <karinamishell.1409@gmail.com>
2023-10-24 17:21:55 -0500

    Karina Cunuhay 17h15

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Karina_Cunuhay_prueba01"
+include(":app")
+ 
